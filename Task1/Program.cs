0c29ad354ba9bbff02e3a89db86ba1c406ff0c19
Mersinres <igor.mmcolor@gmail.com>
2022-12-08 23:11:15 +0300

    ДЗ

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число: ");
+    int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+    int B = Convert.ToInt32(Console.ReadLine());
+
+    if (A > B)
+{
+    Console.WriteLine("Первое число  больше чем второе ");
+}
+else
+{
+    Console.WriteLine("Второе число больше чем первое ");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+{
+    if (A > C)
+    {
+        Console.WriteLine("Максимальное число: " + A);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + C);
+    }
+}
+
+else if (B > C)
+{
+    Console.WriteLine("Максимальное число: " + B);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + C);
+}
